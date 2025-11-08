b93237d366d471ffbe16a41a73f17ae4c0ac7190
KasoziJoel <joelmet256@gmail.com>
2025-11-08 23:23:58 +0300

    Add files via upload

---
+++ gcdl/table-procurement.sql
@@ -0,0 +1,38 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.41 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.12.0.7122
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping structure for table gcdl.procurement
+CREATE TABLE IF NOT EXISTS `procurement` (
+  `Proc_ID` int NOT NULL AUTO_INCREMENT,
+  `Producer_ID` int NOT NULL,
+  `Cost_per_ton` varchar(50) NOT NULL,
+  `Dealer_ID` int NOT NULL,
+  `Branch_ID` int NOT NULL,
+  `Type` varchar(50) NOT NULL,
+  PRIMARY KEY (`Proc_ID`),
+  KEY `FK_procurement_dealers` (`Dealer_ID`),
+  KEY `FK_procurement_branches` (`Branch_ID`),
+  CONSTRAINT `FK_procurement_branches` FOREIGN KEY (`Branch_ID`) REFERENCES `branches` (`Branch_ID`) ON UPDATE RESTRICT,
+  CONSTRAINT `FK_procurement_dealers` FOREIGN KEY (`Dealer_ID`) REFERENCES `dealers` (`Dealer_ID`) ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table gcdl.procurement: ~0 rows (approximately)
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
